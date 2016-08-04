143ae2657a663547f42dbc05d535953f4c99a8a7
EduardoCopat <ecopat@gmail.com>
2016-08-03 22:46:20 -0300

    Adding favicon

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 var express = require('express');
+var favicon = require('serve-favicon');
+var path = require('path');
+
 var app = express();
+
 app.set('port', (process.env.PORT || 3000));
 
-//var Snoocore = require('snoocore');
-//var reddit = new Snoocore();
 
 app.use(express.static(__dirname + '/public'));
 app.use('/bower_components',  express.static(__dirname + '/bower_components'));
+app.use(favicon(path.join(__dirname,'public','favicon.ico')));
 
 app.get('/topnews', function (request, response) {
   response.send('Hello World!');
@@ -14,7 +17,7 @@ app.get('/topnews', function (request, response) {
 
 //To avoid Heroku $PORT error
 app.get('/', function(request, response) {
-  var result = 'App is running'
+  var result = 'App is running';
   response.send(result);
 }).listen(app.get('port'), function() {
   console.log('App is running, server is listening on port ', app.get('port'));
