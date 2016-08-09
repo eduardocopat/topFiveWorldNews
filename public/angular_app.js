394b62fab4e3a3418008d9e419e39ee796b7f699
EduardoCopat <ecopat@gmail.com>
2016-08-08 23:15:08 -0300

    Added a loading spinner

--- public/angular_app.js
+++ public/angular_app.js
@@ -1,10 +1,11 @@
 var app = angular.module('topFive', []);
 app.controller('topFiveController', function($scope, $http) {
     $scope.newsSet = [];
-
+    $scope.loading = true;
     $http.get("/topnews")
         .then(function(response) {
             $scope.newsSet = response.data;
+            $scope.loading = false;
         });
 });
 
